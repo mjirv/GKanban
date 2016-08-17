a1ce09d80d3839d8167f7d83c8373c60a9b1a217
Michael Irvine <michael.irvine@liveramp.com>
2016-08-16 21:21:52 -0700

    Changed styling to match gmail.

--- content_script.js
+++ content_script.js
@@ -1,9 +1,47 @@
-var myKanbanBoard = "https://kanbanflow.com/board/87d521d15744312d8d75fcfdf113cf44";
+var myKanbanBoard = "https://kanbanflow.com/board/87d521d15744312d8d75fcfdf113cf44#board-wrapper";
 
 var iframe = document.createElement('iframe');
 iframe.src = myKanbanBoard;
 iframe.width = "100%";
-iframe.height = "375px";
+iframe.height = "360px";
+iframe.style.border = "0";
+iframe.style.opacity = "0.9";
 
 var container = document.getElementsByClassName('UI')[0];
 container.insertBefore(iframe, container.firstChild);
+
+var h3 = document.createElement('h3');
+h3.className = "Wr";
+
+var hider = document.createElement('img');
+hider.className = "Wp Wq";
+hider.src = "//ssl.gstatic.com/ui/v1/icons/mail/images/cleardot.gif";
+
+h3.appendChild(hider);
+
+h3_div = document.createElement('div');
+h3_div.className = "Wn";
+
+h3_text = document.createElement('span');
+h3_text.className = "qh";
+h3_text.innerText = "Kanban Board";
+h3_div.appendChild(h3_text);
+
+outer_div = document.createElement('div');
+outer_div.className = "nH Wg aAD aAr";
+
+outer_div.onclick = function() {
+	if(iframe.style.display == "none") {
+		iframe.style.display = "";
+		hider.className = "Wp Wq";
+	}
+	else {
+		iframe.style.display = "none";
+		hider.className = "Wp Wo";
+	}
+}
+
+h3.appendChild(h3_div);
+outer_div.appendChild(h3);
+
+container.insertBefore(outer_div, container.firstChild);
