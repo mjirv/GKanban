991bb22bd3fa3adea9b93b3e6b68d743f47ec58e
Michael Irvine <michael.irvine@liveramp.com>
2016-10-14 16:45:11 -0700

    User can configure Kanban ID

---
+++ options.js
@@ -0,0 +1,28 @@
+// Save options to chrome.storage
+function save_options() {
+    var kanban_id = document.getElementById('kanban_id').value;
+    chrome.storage.sync.set({
+        kanbanId: kanban_id
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'ID saved!'
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Default to ID empty
+    chrome.storage.sync.get({
+        kanbanId: ""
+    }, function(items) {
+        document.getElementById('kanban_id').value = items.kanbanId;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
